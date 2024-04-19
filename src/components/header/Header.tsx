114bc87aa03979741afc9dc248e553b5022f4391
ArseniyKhal <asmn@mail.ru>
2024-04-19 23:34:10 +0500

    test4

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { NavMenu } from '../navMenu2/NavMenu';
+import { NavMenu } from '../navMenu/NavMenu';
 import {menuData} from '../../mockData';
 import styles from './Header.module.scss';
 
